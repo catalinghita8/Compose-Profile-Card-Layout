d97dd1740d6d55b3450da37f000d10045f5b037a
Catalin Ghita <cghita@airtouchmedia.com>
2020-12-20 16:10:05 +0200

    Add profile picture composable solution

--- app/src/main/java/com/codingtroops/profilecardlayout/MainActivity.kt
+++ app/src/main/java/com/codingtroops/profilecardlayout/MainActivity.kt
@@ -2,18 +2,21 @@ package com.codingtroops.profilecardlayout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codingtroops.profilecardlayout.ui.ProfileCardLayoutTheme
@@ -30,8 +33,8 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MainScreen() {
     Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = Color.LightGray
+        modifier = Modifier.fillMaxSize(),
+        color = Color.LightGray
     ) {
         ProfileCard()
     }
@@ -40,9 +43,17 @@ fun MainScreen() {
 @Composable
 fun ProfileCard() {
     Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top),
+        elevation = 8.dp
+    ) {
+        Row(
             modifier = Modifier.fillMaxWidth(),
-            elevation = 8.dp) {
-        Row(modifier = Modifier.wrapContentSize()) {
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
             ProfilePicture()
             ProfileContent()
         }
@@ -52,12 +63,23 @@ fun ProfileCard() {
 
 @Composable
 fun ProfilePicture() {
-
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(width = 2.dp, color = Color.Green),
+        modifier = Modifier.padding(16.dp),
+        elevation = 4.dp
+    ) {
+        Image(
+            bitmap = imageResource(id = R.drawable.profile_picture),
+            modifier = Modifier.size(72.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 @Composable
 fun ProfileContent() {
-
+    Text("John Doe")
 }
 
 @Preview(showBackground = true)
