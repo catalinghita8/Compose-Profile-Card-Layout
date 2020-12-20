4569ceb2030e4e534072fe7caf3150082afc96c6
Catalin Ghita <cghita@airtouchmedia.com>
2020-12-20 18:38:34 +0200

    Customize shapes solution

--- app/src/main/java/com/codingtroops/profilecardlayout/ui/Shape.kt
+++ app/src/main/java/com/codingtroops/profilecardlayout/ui/Shape.kt
@@ -1,11 +1,12 @@
 package com.codingtroops.profilecardlayout.ui
 
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val shapes = Shapes(
         small = RoundedCornerShape(4.dp),
-        medium = RoundedCornerShape(4.dp),
+        medium = CutCornerShape(topRight = 24.dp),
         large = RoundedCornerShape(0.dp)
 )
