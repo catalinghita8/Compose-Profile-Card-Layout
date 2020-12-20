7787da106ba63c2da745fb73755ead9431b0153e
Catalin Ghita <cghita@airtouchmedia.com>
2020-12-20 16:48:10 +0200

    Add profile content composable function

--- app/src/main/java/com/codingtroops/profilecardlayout/MainActivity.kt
+++ app/src/main/java/com/codingtroops/profilecardlayout/MainActivity.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Providers
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -79,7 +77,23 @@ fun ProfilePicture() {
 
 @Composable
 fun ProfileContent() {
-    Text("John Doe")
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "John Doe",
+            style = MaterialTheme.typography.h5
+        )
+        Providers(AmbientContentAlpha provides(ContentAlpha.medium)) {
+            Text(
+                text = "Active now",
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
