56ccf1b1a0cc1e3ae2ad954451c1ce95b41979f5
Catalin Ghita <cghita@airtouchmedia.com>
2021-08-26 13:43:19 +0300

    Add lazy column implementation solution - 1.0.1 update

--- app/src/main/java/com/codingtroops/profilecardlayout/MainActivity.kt
+++ app/src/main/java/com/codingtroops/profilecardlayout/MainActivity.kt
@@ -1,23 +1,24 @@
 package com.codingtroops.profilecardlayout
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Providers
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codingtroops.profilecardlayout.ui.MyTheme
@@ -55,7 +56,11 @@ fun MainScreen(userProfiles: List<UserProfile> = userProfileList) {
 fun AppBar() {
     TopAppBar(
         navigationIcon = {
-            Icon(Icons.Default.Home, Modifier.padding(horizontal = 12.dp))
+            Icon(
+                Icons.Default.Home,
+                contentDescription = "",
+                modifier = Modifier.padding(horizontal = 12.dp),
+            )
         },
         title = { Text("Messaging Application users") }
     )
@@ -97,9 +102,10 @@ fun ProfilePicture(drawableId: Int, onlineStatus: Boolean) {
         elevation = 4.dp
     ) {
         Image(
-            bitmap = imageResource(id = drawableId),
+            painter = painterResource(id = drawableId),
             modifier = Modifier.size(72.dp),
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
+            contentDescription = ""
         )
     }
 }
@@ -111,8 +117,8 @@ fun ProfileContent(userName: String, onlineStatus: Boolean) {
             .padding(8.dp)
             .fillMaxWidth()
     ) {
-        Providers(
-            AmbientContentAlpha provides (
+        CompositionLocalProvider(
+            LocalContentAlpha provides (
                     if (onlineStatus)
                         1f else ContentAlpha.medium)
         ) {
@@ -121,7 +127,7 @@ fun ProfileContent(userName: String, onlineStatus: Boolean) {
                 style = MaterialTheme.typography.h5
             )
         }
-        Providers(AmbientContentAlpha provides (ContentAlpha.medium)) {
+        CompositionLocalProvider(LocalContentAlpha provides (ContentAlpha.medium)) {
             Text(
                 text = if (onlineStatus)
                     "Active now"
